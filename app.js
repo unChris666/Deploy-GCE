99c723f6f65878286f68a66db8c9c4fa60fdeb48
ChristianBiran <nathanaelbiran@gmail.com>
2024-11-19 02:46:55 +0700

    update server

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
